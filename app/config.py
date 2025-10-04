eb171d2e2617f121ab39780159818486542157ad
Andrii <andverb@gmail.com>
2025-10-04 14:48:37 +0200

    fix: fix deps for alembic

--- app/config.py
+++ app/config.py
@@ -1,12 +1,12 @@
 from pydantic import Field, field_validator
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     database_url: str = Field(description="Database URL")
     api_key: str = Field(description="API key for access to write delete and update routes")
     environment: str = Field(default="development", description="Environment: development, staging, production")
-    # model_config = SettingsConfigDict(env_file=".env", env_prefix="gnosis_")
+    model_config = SettingsConfigDict(env_file=".env")
 
     @field_validator("database_url")
     @classmethod
